7c3c47bbc4b1e008aea0e884a6abba1b64acdeee
ReySun <reysun_sky@outlook.com>
2023-06-23 23:06:09 +0800

    feat: update Timer 方法和参数的使用

--- dart_async/timer_class/constructors/timer.dart
+++ dart_async/timer_class/constructors/timer.dart
@@ -1,8 +1,21 @@
 import 'dart:async';
 
-main() {
+main() async {
   final timer =
       Timer(const Duration(seconds: 5), () => print('Timer finished'));
 
-  print(timer);
+  print('timer 是否激活状态：' +
+      timer.isActive.toString() +
+      '\ntick = ' +
+      timer.tick.toString());
+
+  await Future.delayed(Duration(seconds: 5));
+
+  // 你也可以提前取消timer
+  // timer.cancel;
+
+  print('5秒之后 timer 是否激活状态：' +
+      timer.isActive.toString() +
+      '\ntick = ' +
+      timer.tick.toString());
 }
